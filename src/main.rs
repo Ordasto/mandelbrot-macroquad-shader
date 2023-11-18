1ea25d0b204661ad584e4ae96a40a9757929378e
Ordo <sam.peters24@gmail.com>
2023-11-18 23:26:26 +0000

    proper zooming

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use macroquad::{
 
 #[macroquad::main("mandelbrot-set")]
 async fn main() {
-    let texture = render_target(1000, 1000).texture;
+    let texture = render_target(100, 100).texture;
     let material = load_material(
         ShaderSource::Glsl {
             vertex: VERTEX_SHADER,
@@ -16,6 +16,8 @@ async fn main() {
                 ("screen_size".to_owned(), UniformType::Float2),
                 ("position".to_owned(), UniformType::Float2),
                 ("zoom".to_owned(), UniformType::Float1),
+                ("max_iterations".to_owned(), UniformType::Int1),
+                ("time".to_owned(), UniformType::Float1),
             ],
             ..Default::default()
         },
@@ -24,7 +26,7 @@ async fn main() {
 
     let mut pos_x: f32 = 0.0;
     let mut pos_y: f32 = 0.0;
-    let mut speed: f32 = 0.55;
+    let speed: f32 = 0.55;
     let mut zoom: f32 = 1.0;
 
     let mut mandel_inc = 1;
@@ -33,23 +35,25 @@ async fn main() {
     loop {
         // move this stuff somewhere else
         let dt = get_frame_time();
+        // speed for this frame (factoring in delta time and zoom)
+        let frame_speed = (speed * zoom) * dt;
         if is_key_down(KeyCode::A) {
-            pos_x -= speed * dt;
+            pos_x -= frame_speed;
         }
         if is_key_down(KeyCode::D) {
-            pos_x += speed * dt;
+            pos_x += frame_speed;
         }
         if is_key_down(KeyCode::W) {
-            pos_y += speed * dt;
+            pos_y += frame_speed;
         }
         if is_key_down(KeyCode::S) {
-            pos_y -= speed * dt;
+            pos_y -= frame_speed;
         }
-        if is_key_pressed(KeyCode::Q) {
-            zoom -= 0.05;
+        if is_key_down(KeyCode::Q) {
+            zoom -= 0.5 * zoom * dt;
         }
-        if is_key_pressed(KeyCode::E) {
-            zoom += 0.05;
+        if is_key_down(KeyCode::E) {
+            zoom += 0.5 * zoom * dt;
         }
 
         clear_background(WHITE);
@@ -58,7 +62,9 @@ async fn main() {
         material.set_uniform("screen_size", (screen_width(), screen_height()));
         material.set_uniform("position", (pos_x, pos_y));
         material.set_uniform("zoom", zoom);
-
+        material.set_uniform("max_iterations", 100);
+        material.set_uniform("time", get_time() as f32);
+        println!("{}", get_time());
         draw_texture_ex(
             &texture,
             0.0,
@@ -86,37 +92,30 @@ async fn main() {
 const FRAGMENT_SHADER: &str = r#"#version 100
 precision highp float;
 
+uniform sampler2D Texture;
 uniform vec2 screen_size;
 uniform vec2 position;
 uniform float zoom;
+uniform int max_iterations;
+uniform float time;
 
 void main() {
     gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
 
-// vec2 coord = gl_FragCoord.xy / screen_size.xy;
-//     vec2 scaled = vec2(
-//         coord.x * (0.47 - -2.00) + -2.00,
-//         coord.y * (1.12 - -1.12) + -1.12
-//     );
-//
     vec2 scaled = (2.0*gl_FragCoord.xy - screen_size.xy) / screen_size.y;
 
+    scaled *= zoom;
     scaled.x += position.x;
     scaled.y += position.y;
-    scaled *= zoom;
 
     float x = 0.0;
     float y = 0.0;
     float tmp = 0.0;
     
-    // make this a uniform
-    int iter_max = 1000;
     int i = 0;
 
-
-
-    for(; i < iter_max; i++){
-        if(x*x + y*y > 4.0){
+    for(; i < max_iterations*int(time); i++){
+        if(x*x + y*y > 4.0 ){
             break;
         }
         
@@ -126,14 +125,15 @@ void main() {
     }
 
     float fi = float(i);
-    float i_norm = (float(i)/float(iter_max)); 
+    float i_norm = (float(i)/float(max_iterations)); 
 
     // need to figure out an actual good method of coloring 
-    if(i < iter_max){
+    if(i < max_iterations){
         gl_FragColor.r = i_norm * fi;
         gl_FragColor.g = i_norm * fi/2.0;
         gl_FragColor.b = i_norm * fi/2.0;
     }
+    
 }
 
 "#;
